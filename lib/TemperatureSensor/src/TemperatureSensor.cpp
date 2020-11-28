0a8389f92ac44139619e78c4b340f26af280d88b
shakram02 <ahmedhamdyau@gmail.com>
2020-11-28 19:50:20 +0200

    Adjust temperature library for 10-bit / 8-bit controllers

--- lib/TemperatureSensor/src/TemperatureSensor.cpp
+++ lib/TemperatureSensor/src/TemperatureSensor.cpp
@@ -1,12 +1,15 @@
+// Source: https://learn.adafruit.com/thermistor/using-a-thermistor
 #include "TemperatureSensor.h"
 #define CONST_RESISTOR 10000
 #define THERMISTORNOMINAL 10000
 #define TEMPERATURENOMINAL 25
 #define BCOEFFICIENT 3950
+#define ANALOG_MAX_08_BIT 255.0
+#define ANALOG_MAX_10_BIT 1023.0
 
 float TemperatureSensor::getTemperature(unsigned short pinValue)
 {
-    float resistance = CONST_RESISTOR / ((1023.0 / pinValue) - 1);
+    float resistance = CONST_RESISTOR / ((ANALOG_MAX_10_BIT / pinValue) - 1);
 
     float steinhart = resistance / THERMISTORNOMINAL; // (R/Ro)
     steinhart = log(steinhart);                       // ln(R/Ro)
